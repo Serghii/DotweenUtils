b79e7db605f2b6033391b77deccd39d58566ba18
Serhii <s.safonov@room8studio.com>
2022-04-23 21:53:50 +0300

    init

---
+++ Utility/Utils.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace DotweenStuff
+{
+    public static class Utils
+    {
+        public static TweenerCore<Color, Color, ColorOptions> AlphaOut(this Graphic target, float duration, float toAlpha)
+        {
+            return ChangeAlpha(target,1,toAlpha,duration).SetEase(Ease.OutSine);
+        }
+        
+        public static TweenerCore<Color, Color, ColorOptions> AlphaOut(this Graphic target,float duration)
+        {
+            return ChangeAlpha(target,1,0,duration).SetEase(Ease.OutSine);
+        }
+        
+        public static TweenerCore<Color, Color, ColorOptions> AlphaIn(this Graphic target, float duration)
+        {
+            return ChangeAlpha(target,0,1,duration).SetEase(Ease.OutSine);
+        }
+        
+        public static TweenerCore<Color, Color, ColorOptions> AlphaIn(this Graphic target, float duration,float toAlpha)
+        {
+            return ChangeAlpha(target,0,toAlpha,duration).SetEase(Ease.OutSine);
+        }
+        
+        public static TweenerCore<Color, Color, ColorOptions> ChangeAlpha(this Graphic target, float startValue, float endValue,float duration)
+        {
+            Color startColor = target.color;
+            startColor.a = startValue;
+        
+            Color endColor = target.color;
+            endColor.a = endValue;
+        
+            target.color = startColor;
+            return  target.DOColor(endColor, duration);
+        }
+
+    }
+}
